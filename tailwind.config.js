3eb672ff518e93a4e1947cac9822cb1ec704d812
rray524 <rray524@gmail.com>
2024-08-10 22:29:28 +0530

    add-tailwind-css-to-project-1.1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-export const content = ["./src/**/*.{js,jsx,ts,tsx}"];
+export const content = ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"];
 export const theme = {
   extend: {},
 };
