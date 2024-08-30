2bf55d46eef191d425d5c79755b2e9b971f8b979
Adewale James Adesina <adesinaadewale28@yahoo.com>
2024-08-30 11:49:43 +0100

    Update tailwind.config.js
    extend open sans font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export const content = ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"];
 export const theme = {
-  extend: {},
+  extend: {
+    fontFamily: {
+      sans: ['Open Sans', 'ui-sans-serif', 'system-ui'],
+    },
+  },
 };
 export const plugins = [];
