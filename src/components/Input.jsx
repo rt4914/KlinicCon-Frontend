4dbc5ecc56728f2075a8ebc88ca0fb946c452189
Aniket Patidar <aniketpatidar01@gmail.com>
2024-09-10 01:34:51 +0530

    Refactor Input component v1

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -3,22 +3,30 @@ const Input = ({
   name,
   type = 'text',
   placeholder,
-  required,
-  borderColor = 'border-gray-300',
-  focusBorderColor = 'focus:border-gray-500',
-  backgroundColor = 'bg-white',
-  textColor = 'text-gray-900',
-  placeholderColor = 'placeholder-gray-400',
-  labelColor = 'text-gray-900',
-  labelFontWeight = 'font-normal',
-  labelClassName = '',
-  inputClassName = '',
-  wrapperClassName = ''
+  required = false,
+  variant = 'DEFAULT'
 }) => {
+  const variants = {
+    DEFAULT: {
+      input: 'border-gray-500 text-gray-900',
+      label: 'text-gray-900 text-sm',
+    },
+    SECONDARY: {
+      input: 'bg-gray-100 border-gray-300 text-gray-700',
+      label: 'text-gray-500 text-sm',
+    },
+    SMALL: {
+      input: 'text-sm border-gray-300 text-gray-700',
+      label: 'font-semibold text-xs',
+    },
+  };
+
+  const { input: inputStyles, label: labelStyles } = variants[variant] || variants.DEFAULT;
+
   return (
-    <div className={`${wrapperClassName}`}>
+    <div className="mb-4">
       {label && (
-        <label htmlFor={name} className={`block mb-1 text-sm ${labelColor} ${labelFontWeight} ${labelClassName}`}>
+        <label htmlFor={name} className={`block mb-2 tracking-tight ${labelStyles}`}>
           {label}
           {required && <span>*</span>}
         </label>
@@ -29,8 +37,7 @@ const Input = ({
         id={name}
         placeholder={placeholder}
         required={required}
-        className={`px-3 py-2 border rounded-lg focus:outline-none
-          ${borderColor} ${focusBorderColor} ${backgroundColor} ${textColor} ${placeholderColor} ${inputClassName}`}
+        className={`block rounded-md border px-3 py-2 focus:outline-none text-xs w-full ${inputStyles}`}
       />
     </div>
   );
