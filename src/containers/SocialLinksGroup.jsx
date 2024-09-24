087869803cefc92acd8987a45d5e8c8d97013bfc
Vanam <raghavendra.gv.vanam@gmail.com>
2024-09-25 00:21:23 +0530

    Removed provided width and height, social link displayed with default width and height

--- src/containers/SocialLinksGroup.jsx
+++ src/containers/SocialLinksGroup.jsx
@@ -3,9 +3,9 @@ import SocialLink from "../components/SocialLink";
 const SocialLinksGroup = () => {
     return (
         <div className="flex justify-center">
-            <SocialLink width={12} height={12} iconText="instagram"/>
-            <SocialLink width={12} height={12} iconText="twitter"/>
-            <SocialLink width={12} height={12} iconText="facebook"/>
+            <SocialLink iconText="instagram"/>
+            <SocialLink iconText="twitter"/>
+            <SocialLink iconText="facebook"/>
         </div>
     )
 }
