c4cc1f6cb6c136d55a54e1a0d000e175aa1232f6
Vanam <raghavendra.gv.vanam@gmail.com>
2024-09-26 11:00:58 +0530

    Below are the changes 1.Fixed as per PR review changed the approach and passed three props for configuring fb, twitter & instagram and also URL for Link component

--- src/components/SocialLink.jsx
+++ src/components/SocialLink.jsx
@@ -1,29 +1,26 @@
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import {Link} from "react-router-dom";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 
-const SocialLink = ({width, height, iconText}) => {
-    return (
-        <div>
-            <Link  to={"#"}>
-                    <div className={`m-2 border border-neutral-900 rounded-full flex justify-center items-center hover:bg-neutral-900 hover:text-white w-${width} h-${height}`}>
-                        <i className={`text-2xl fa-brands fa-square-${iconText}`}></i>
-                    </div>
-                </Link>
+const SocialLink = ({ icon, href, text }) => {
+  return (
+    <div>
+      <Link to={href}>
+        <div
+          className={`m-2 border border-neutral-900 rounded-full 
+          flex justify-center items-center hover:bg-neutral-900 hover:text-white w-12 h-12`}
+        >
+          <i className={`text-2xl ${icon}`}></i>
         </div>
-    );
-}
+      </Link>
+    </div>
+  );
+};
 
 SocialLink.propTypes = {
-    width: PropTypes.number,
-    height: PropTypes.number,
-    iconText: PropTypes.string
-}
-
-SocialLink.defaultProps = {
-    width: 12,
-    height: 12,
-    iconText: "instagram"
-}
+  icon: PropTypes.string,
+  href: PropTypes.string,
+  text: PropTypes.string,
+};
 
 export default SocialLink;
--- src/containers/SocialLinksGroup.jsx
+++ src/containers/SocialLinksGroup.jsx
@@ -1,13 +1,25 @@
 import SocialLink from "../components/SocialLink";
 
 const SocialLinksGroup = () => {
-    return (
-        <div className="flex justify-center">
-            <SocialLink iconText="instagram"/>
-            <SocialLink iconText="twitter"/>
-            <SocialLink iconText="facebook"/>
-        </div>
-    )
-}
+  return (
+    <div className="flex justify-center">
+      <SocialLink
+        icon="fa-brands fa-square-instagram"
+        href={"#"}
+        text={"Instagram"}
+      />
+      <SocialLink
+        icon="fa-brands fa-square-twitter"
+        href={"#"}
+        text={"Twitter"}
+      />
+      <SocialLink
+        icon="fa-brands fa-square-facebook"
+        href={"#"}
+        text={"Facebook"}
+      />
+    </div>
+  );
+};
 
 export default SocialLinksGroup;
