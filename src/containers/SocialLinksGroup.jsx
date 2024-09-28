9c0b00c96f1b25366505832772babc563a0dea72
Vanam <raghavendra.gv.vanam@gmail.com>
2024-09-28 13:17:05 +0530

    Below are the changes 1.Changed the name of the variable used in map method function based on review comments

--- src/containers/SocialLinksGroup.jsx
+++ src/containers/SocialLinksGroup.jsx
@@ -4,32 +4,32 @@ const socialLinksData = [
   {
     icon: "fa-brands fa-square-instagram",
     href: "#",
-    text: "Instagram"
+    text: "Instagram",
   },
   {
     icon: "fa-brands fa-square-twitter",
     href: "#",
-    text: "Twitter"
+    text: "Twitter",
   },
   {
     icon: "fa-brands fa-square-facebook",
     href: "#",
-    text: "Facebook"
-  }
-]
-
+    text: "Facebook",
+  },
+];
 
 const SocialLinksGroup = () => {
-  const dataItems= socialLinksData.map((scLink, idx) => {
+  const dataItems = socialLinksData.map((socialLink) => {
     return (
-       <SocialLink icon={scLink.icon} href={scLink.href} text={scLink.text} key={idx}/>
-    )
-  })
-  return (
-    <div className="flex justify-center gap-2">
-      {dataItems}
-    </div>
-  );
+      <SocialLink
+        icon={socialLink.icon}
+        href={socialLink.href}
+        text={socialLink.text}
+        key={socialLink.text}
+      />
+    );
+  });
+  return <div className="flex justify-center gap-2">{dataItems}</div>;
 };
 
 export default SocialLinksGroup;
