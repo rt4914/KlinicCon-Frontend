42e150012988c2f7ac59045aaf6bf9d0620e67da
Adewale James Adesina <adesinaadewale28@yahoo.com>
2024-10-05 09:42:56 +0100

    Fix #29: Update tailwind.config.js
    re-indenting code

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,10 +11,10 @@ export default {
     extend: {
       colors: {
         ...defaultColors,
-        'orange':{
+        'orange': {
           DEFAULT: '#ff6725',
-        'dark': '#ff3f25',
-        'light': '#fef7f3',
+          'dark': '#ff3f25',
+          'light': '#fef7f3',
         },
         'brown': '#453a34',
         'green': '#00b579',
