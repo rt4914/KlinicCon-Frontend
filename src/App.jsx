e8b1638cd93c2580544153c121a3f5d939cfd080
Adewale James Adesina <adesinaadewale28@yahoo.com>
2024-10-05 09:34:36 +0100

    Fix #29: Update App.jsx
    returns only hello world

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,5 @@
-const App = () => {
-  return (
-<<<<<<< HEAD
-    <div>
-    <h1 className="text-orange bg-brown text-4xl font-bold">
-      Hello World!
-    </h1>
-      <p className="text-orange-dark"> Sign In </p>
-    </div>
-=======
-    <>
-      <h1 className="underline p-4">Hello World!</h1>
-    </>
->>>>>>> origin/main
-  );
-};
-
-export default App;
+<h1 className="text-orange bg-brown text-4xl font-bold">
+  Hello World!
+</h1>
+    
+    
