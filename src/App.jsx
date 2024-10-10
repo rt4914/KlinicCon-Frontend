0647e9fcbbf2b9d3c405d250db023da114ac21e9
Adewale James Adesina <adesinaadewale28@yahoo.com>
2024-10-10 09:22:49 +0100

    Fixes #29: Update App.jsx
    reformat and added whitespace at the bottom

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
-<h1 className="text-orange bg-brown text-4xl font-bold">
-  Hello World!
-</h1>
-    
-    
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-orange bg-brown text-4xl font-bold">
+        Welcome to KlinicCon App
+      </h1>
+    </div>
+  );
+};
+
+export default App;
